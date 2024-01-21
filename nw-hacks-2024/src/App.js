48bc695b7216143a5d0e51b5823881ebf192aa7f
Evan Nawfal <evanxnawfal@gmail.com>
2024-01-20 16:50:24 -0800

    Fixed maybe?

--- nw-hacks-2024/src/App.js
+++ nw-hacks-2024/src/App.js
@@ -1,20 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header'; // Assuming Header is the component for your site header
-import { Switch, Route } from 'react-router-dom';
-import Login from './Login'; // Assuming Login and Events are components for your routes
-import Events from './Events';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <main className="App-main">
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/events" component={Events} />
-        </Switch>
-      </main>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
