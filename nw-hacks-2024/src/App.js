6f9df70aa5982891a925736c5a746dfe22eb49fd
Evan Nawfal <evanxnawfal@gmail.com>
2024-01-21 09:55:53 -0800

    firebase test

--- nw-hacks-2024/src/App.js
+++ nw-hacks-2024/src/App.js
@@ -3,12 +3,12 @@ import { Login } from './Login.js';
 import { Events } from './Events.js';
 import { Navbar } from "./Navbar.js";
 
-
-
 export function App() {
 
   return (
     <div className="App">
+      <script src="https://www.gstatic.com/firebasejs/10.7.2/firebase-app-compat.js"></script>
+      <script src="https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore-compat.js"></script>
       <Router>
         <Navbar />
         <Routes>
--- nw-hacks-2024/src/firebase.js
+++ nw-hacks-2024/src/firebase.js
@@ -1,31 +1,37 @@
-import {initializeApp} from 'firebase/app';
-import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword } from 'firebase/auth';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC82G0eW8Sphq2Q6TuK0__uXvkJToXJ0OY",
-    authDomain: "nwhacks2024-affe8.firebaseapp.com",
-    projectId: "nwhacks2024-affe8",
-    storageBucket: "nwhacks2024-affe8.appspot.com",
-    messagingSenderId: "732641950384",
-    appId: "1:732641950384:web:90161413440142633a9975",
-    measurementId: "G-5TC78VY487"
+  apiKey: "AIzaSyC82G0eW8Sphq2Q6TuK0__uXvkJToXJ0OY",
+  authDomain: "nwhacks2024-affe8.firebaseapp.com",
+  projectId: "nwhacks2024-affe8",
+  storageBucket: "nwhacks2024-affe8.appspot.com",
+  messagingSenderId: "732641950384",
+  appId: "1:732641950384:web:90161413440142633a9975",
+  measurementId: "G-5TC78VY487"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
 
-const signInWithGoogle = () => {
-    signInWithPopup(auth, provider).then((result) => {
-        const name = result.user.displayName;
-        const email = result.user.email;
-        const profilePic = result.user.photoURL;
+const signInWithGoogle = async () => {
+  try {
+    const result = await signInWithPopup(auth, provider);
+    const emailAddress = result.user.email;
+    const profilePic = result.user.photoURL;
 
-        localStorage.setItem("name", name);
-        localStorage.setItem("email", email);
-        localStorage.setItem("profilePic", profilePic);
-    }).catch((error) => {
-        console.log(error);
-    });
+    const usersCollection = collection(db, 'users');
+    const userEntity = { Email: emailAddress};
+    await addDoc(usersCollection, userEntity);
+
+   // console.log("Document written with ID: ", docRef.id);
+  } catch (error) {
+    console.error("Error signing in or adding document: ", error);
+  }
 };
-export {signInWithGoogle, auth, app};
+
+export { signInWithGoogle, auth, app };
